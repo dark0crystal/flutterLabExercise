9c4ac02910a5f06c69364ef5685f54b65553e60e
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-29 00:51:59 +0400

    computer post style

--- lib/ComputerWidget.dart
+++ lib/ComputerWidget.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/computer.dart';
 
 class ComputerWidget extends StatelessWidget {
-  const ComputerWidget({super.key});
+  final Computer computer;
+
+  const ComputerWidget({super.key, required this.computer});
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Container(
-        child: Text("ComputerWidget"),
-      )
-    ],);
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(computer.imagePath, height: 100),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(computer.modelName, style: const TextStyle(fontWeight: FontWeight.bold)),
+                const SizedBox(height: 4),
+                Text('\$${computer.price.toString()}'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
