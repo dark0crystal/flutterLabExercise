03bf5a7c4787f671590a2d2adec6fa36eed2d5d5
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-29 02:39:27 +0400

    find the cheepst computer

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:lab2/computersGrid.dart';
+import 'package:lab2/computer.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  void findCheapestComputer(BuildContext context) {
+    final cheapest = computers.reduce((a, b) => a.price < b.price ? a : b);
+    MaterialBannerMessenger(context, "Cheapest: ${cheapest.modelName} - \$${cheapest.price}");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: ComputersGrid(),
+    return Column(
+      children: [
+        const Expanded(child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ComputersGrid(),
+        )),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () => findCheapestComputer(context),
+            child: const Text("Find Cheapest Computer"),
+          ),
+        )
+      ],
     );
   }
 }
