bf9c748f6aa9230bb08cc66805a185e410727c66
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-29 02:39:47 +0400

    add menu for all computers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:lab2/home.dart';
+import 'package:lab2/computer.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Computer Store"),
-        backgroundColor: Colors.amberAccent,
+    home: Builder(
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Computer Store"),
+          backgroundColor: Colors.amberAccent,
+          leading: PopupMenuButton(
+            icon: Icon(Icons.menu),
+            itemBuilder: (context) => computers
+                .map((computer) => PopupMenuItem(
+                      value: computer,
+                      child: Text(computer.modelName),
+                    ))
+                .toList(),
+            onSelected: (computer) => SnackBarMessenger(context, "${computer.modelName} - \$${computer.price}"),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.info),
+              onPressed: () => MaterialBannerMessenger(context, "This app helps you browse available computers. Tap an image for details."),
+            ),
+          ],
+        ),
+        body: const Home(),
       ),
-      body: const Home(),
     ),
   ));
 }
