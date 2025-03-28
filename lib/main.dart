655c05334dce5f34969f2524683ce29f74c3c0a5
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-28 23:10:43 +0400

    adding a computer class

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,16 @@ void main() {
         title:const Text("Helloo world"),
         backgroundColor: Colors.amberAccent,
       ),
+      body:Text("This is the body")
     )
   ));
+}
+
+
+class Computer {
+  final String modelName;
+  final double price;
+  final String imagePath;
+
+  Computer({required this.modelName, required this.price, required this.imagePath});
 }
