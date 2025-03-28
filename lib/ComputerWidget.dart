6df14f625624b6eaea10dec84bbc2b6d649e9206
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-29 00:28:04 +0400

    seperating and creating a seperate componrent for computers post

---
+++ lib/ComputerWidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ComputerWidget extends StatelessWidget {
+  const ComputerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Container(
+        child: Text("ComputerWidget"),
+      )
+    ],);
+  }
+}
